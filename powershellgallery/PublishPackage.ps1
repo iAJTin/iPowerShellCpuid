11ffffed8c1f44893e09307547c5296463668143
Fernando Garcia <fdo.garcia.vega@gmail.com>
2020-10-15 21:06:44 +0200

    Update PublishPackage.ps1

--- powershellgallery/PublishPackage.ps1
+++ powershellgallery/PublishPackage.ps1
@@ -1,2 +1,2 @@
 
-Publish-Module -Path ..\src\iPowerShellCpuid\bin\Release\iPowerShellCpuid -Repository PSGallery -NuGetApiKey "oy2dwkgenqul6ayym43qbcz22chswtpu3dtgz5gtdeaobi"
+Publish-Module -Path ..\src\iPowerShellCpuid\bin\Release\iPowerShellCpuid -Repository PSGallery -NuGetApiKey ""
