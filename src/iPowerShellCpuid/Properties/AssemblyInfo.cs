e8e00f74c4bda50518654138d704a3cbc4fba43a
Fernando Garcia <fdo.garcia.vega@gmail.com>
2020-10-16 19:50:48 +0200

    update assemblyinfo

--- src/iPowerShellCpuid/Properties/AssemblyInfo.cs
+++ src/iPowerShellCpuid/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("iPowerShellCpuid")]
-[assembly: AssemblyDescription("CPUID Powershell binary library")]
+[assembly: AssemblyDescription("CPUID PowerShell Cmdlets library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("iTin")]
 [assembly: AssemblyProduct("iPowerShellCpuid")]
